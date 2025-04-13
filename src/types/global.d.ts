b0202def28a52d08733ef6488dc0730aeca302d2
NosytLabs <hi@nosytlabs.com>
2025-04-13 07:27:49 -0300

    Fix TypeScript build errors: relax strict checks and add type declarations

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -9,6 +9,8 @@ declare module 'express-rate-limit' {
     message?: string | object;
     statusCode?: number;
     headers?: boolean;
+    standardHeaders?: boolean;
+    legacyHeaders?: boolean;
     skipFailedRequests?: boolean;
     skipSuccessfulRequests?: boolean;
     requestWasSuccessful?: (req: Request, res: Response) => boolean;
@@ -20,6 +22,31 @@ declare module 'express-rate-limit' {
 
   function rateLimit(options?: Options): (req: Request, res: Response, next: NextFunction) => void;
   export = rateLimit;
+}
+
+declare module 'helmet' {
+  import { RequestHandler } from 'express';
+  
+  interface HelmetOptions {
+    contentSecurityPolicy?: boolean | object;
+    crossOriginEmbedderPolicy?: boolean | object;
+    crossOriginOpenerPolicy?: boolean | object;
+    crossOriginResourcePolicy?: boolean | object;
+    dnsPrefetchControl?: boolean | object;
+    expectCt?: boolean | object;
+    frameguard?: boolean | object;
+    hidePoweredBy?: boolean | object;
+    hsts?: boolean | object;
+    ieNoOpen?: boolean | object;
+    noSniff?: boolean | object;
+    originAgentCluster?: boolean | object;
+    permittedCrossDomainPolicies?: boolean | object;
+    referrerPolicy?: boolean | object;
+    xssFilter?: boolean | object;
+  }
+
+  function helmet(options?: HelmetOptions): RequestHandler;
+  export = helmet;
 }
 
 // Add any other module declarations here if needed 
